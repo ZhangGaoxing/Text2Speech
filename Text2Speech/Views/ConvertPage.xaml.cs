3b094dfa3b99a958da1dc1ef8a5cf0b16c740e6f
Zhang Yuexin <zhangyuexin121@live.cn>
2018-02-04 21:24:34 +0800

    Word Boundary

--- Text2Speech/Views/ConvertPage.xaml.cs
+++ Text2Speech/Views/ConvertPage.xaml.cs
@@ -19,6 +19,9 @@ using Text2Speech.Managers;
 using System.Threading.Tasks;
 using Windows.UI;
 using Windows.ApplicationModel.Core;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Core;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -129,8 +132,13 @@ namespace Text2Speech.Views
                         }
                         SpeechSynthesisStream synthesisStream = await synthesizer.SynthesizeTextToStreamAsync(text);
 
+                        var mediaSource = MediaSource.CreateFromStream(synthesisStream, synthesisStream.ContentType); 
+                        var mediaPlaybackItem = new MediaPlaybackItem(mediaSource);  
+                        RegisterForWordBoundaryEvents(mediaPlaybackItem);
+
                         Media.AutoPlay = true;
-                        Media.SetSource(synthesisStream, synthesisStream.ContentType);
+                        //Media.SetSource(synthesisStream, synthesisStream.ContentType);
+                        Media.SetPlaybackSource(mediaPlaybackItem);
                         Media.Play();
 
                         ConvertProgress.IsActive = false;
@@ -196,6 +204,8 @@ namespace Text2Speech.Views
 
         private void SetSpeechOptions()
         {
+            ConvertText.SelectionHighlightColorWhenNotFocused = new SolidColorBrush(Colors.Blue);
+            synthesizer.Options.IncludeWordBoundaryMetadata = true;
             synthesizer.Options.AudioPitch = ConvertPitch.Value * 2 / 100.0;
             synthesizer.Options.AudioVolume = ConvertVolume.Value / 100.0;
             synthesizer.Options.SpeakingRate = ConvertRate.Value * 6 / 100.0;
@@ -213,5 +223,65 @@ namespace Text2Speech.Views
                 }
             }
         }
+
+        #region WordBoundary
+        private void RegisterForWordBoundaryEvents(MediaPlaybackItem mediaPlaybackItem)
+        {
+            for (int index = 0; index < mediaPlaybackItem.TimedMetadataTracks.Count; index++)
+            {
+                RegisterMetadataHandlerForWords(mediaPlaybackItem, index);
+            }
+
+            mediaPlaybackItem.TimedMetadataTracksChanged += (MediaPlaybackItem sender, IVectorChangedEventArgs args) =>
+            {
+                if (args.CollectionChange == CollectionChange.ItemInserted)
+                {
+                    RegisterMetadataHandlerForWords(sender, (int)args.Index);
+                }
+                else if (args.CollectionChange == CollectionChange.Reset)
+                {
+                    for (int index = 0; index < sender.TimedMetadataTracks.Count; index++)
+                    {
+                        RegisterMetadataHandlerForWords(sender, index);
+                    }
+                }
+            };
+        }
+
+        private void RegisterMetadataHandlerForWords(MediaPlaybackItem mediaPlaybackItem, int index)
+        {
+            var timedTrack = mediaPlaybackItem.TimedMetadataTracks[index];
+
+            if (timedTrack.Id == "SpeechWord")
+            {
+                timedTrack.CueEntered += Metadata_SpeechCueEntered;
+                mediaPlaybackItem.TimedMetadataTracks.SetPresentationMode((uint)index, TimedMetadataTrackPresentationMode.ApplicationPresented);
+            }
+        }
+
+        private async void Metadata_SpeechCueEntered(TimedMetadataTrack timedMetadataTrack, MediaCueEventArgs args)
+        {
+            if (timedMetadataTrack.TimedMetadataKind == TimedMetadataKind.Speech)
+            {
+                var cue = args.Cue as SpeechCue;
+                if (cue != null)
+                {
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                     () =>
+                     {
+                         HighlightTextOnScreen(cue.StartPositionInInput, cue.EndPositionInInput);
+                     });
+                }
+            }
+        }
+
+        private void HighlightTextOnScreen(int? startPositionInInput, int? endPositionInInput)
+        {
+            if (startPositionInInput != null && endPositionInInput != null)
+            {
+                ConvertText.Select(startPositionInInput.Value, endPositionInInput.Value - startPositionInInput.Value + 1);
+            }
+        }
+        #endregion
     }
 }
